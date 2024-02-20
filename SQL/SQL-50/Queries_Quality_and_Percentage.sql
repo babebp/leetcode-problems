cfb9d06a29c7b29a5c167d35d1ae3637767bafa8
babebp <tpacharawut@hotmail.com>
2024-02-20 20:01:21 +0700

    finish Queries Quality and Percentage

---
+++ SQL/SQL-50/Queries_Quality_and_Percentage.sql
@@ -0,0 +1,5 @@
+select query_name, round(sum(rating / position) / count(query_name), 2) as quality ,
+round(sum(case when rating < 3 then 1 else 0 end) / count(query_name) * 100 , 2) as poor_query_percentage
+from Queries
+where query_name is not null
+group by query_name;
